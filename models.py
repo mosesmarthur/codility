10b6c5b11a307e6142a98066bc0d99452bdd804c
mosesmarthur <mosesarthur@outlook.dk>
2022-01-23 15:23:33 +0100

    picking attributes by tree

--- models.py
+++ models.py
@@ -1,49 +1,57 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from pyparsing import alphas
-from sklearn import linear_model
 import cnn 
+import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
-import numpy as np
+from sklearn import linear_model
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-
-
+from sklearn.ensemble import ExtraTreesClassifier
 
 data_train = pd.read_csv('data_train.csv')
 data_test = pd.read_csv('data_test.csv')
+x_train = data_train.iloc[:,0:98]
+y_train = data_train['price']
 
-def pca(data_train):
-    ndata_train = StandardScaler().fit_transform(data_train) # normalizing the features
+x_test = data_test.iloc[:,0:98]
+y_test = data_test['price']
+
+
+def visualize(d1,d2):
+    model = ExtraTreesClassifier()
+    model.fit(d1,d2)
+    #print(model.feature_importances_) #use inbuilt class feature_importances of tree based classifiers
+    #plot graph of feature importances for better visualization
+    feat_importances = pd.Series(model.feature_importances_, index=d1.columns)
+    feat_importances.nlargest(10).plot(kind='barh')
+    feat_importances = feat_importances.nlargest(10)    
+    #plt.show()
+    return d1[feat_importances.keys()]
+
+def pca(d1):
+    ndata_train = StandardScaler().fit_transform(d1) # normalizing the features
     ndata_train = pd.DataFrame(ndata_train)
-    pca_data= PCA(n_components=25)
+    pca_data= PCA(n_components=35)
     pca_data_train = pca_data.fit_transform(ndata_train)
     pca_data_train = pd.DataFrame(pca_data_train)
+    print(pca_data_train)
     return pca_data_train
 
-def correlationmatrix(data_train):
-    data_train_cor = data_train.corr() # Generate correlation matrix
-    fig = go.Figure()
-    fig.add_trace(
-        go.Heatmap(
-            x = data_train_cor.columns,
-            y = data_train_cor.index,
-            z = np.array(data_train_cor)
-        )
-    )
-    fig.show()
-
 def linear(x_train,y_train):
+    print('this is x train',x_train)
+    print('this is y train',y_train)
     #Linear Regression 
     reg = linear_model.LinearRegression()
     reg.fit(x_train, y_train)
     result = {}
     result['score'] = reg.score(x_train, y_train)
-    result['Coeff'] = reg.coef_    
-    return(result)
+    result['Coeff'] = reg.coef_  
+    print('This is Linear',result)  
+    return result
 
 def ridge(x_train,y_train):
     #Ridge CV
@@ -53,19 +61,15 @@ def ridge(x_train,y_train):
     result = {}
     result['score'] = reg.score(x_train, y_train)
     result['Coeff'] = reg.coef_    
-    return(result)
+    return result
     
 def main():
     
-    x_train = data_train.iloc[:,0:98]
-    y_train = data_train['price']
-
-    x_test = data_test.iloc[:,0:98]
-    y_test = data_test['price']
-
-    npcadata_train = pca(data_train)
-    correlationmatrix(npcadata_train)
-    #linear_result = linear(x_train,y_train)
+    rdata_train = visualize(x_train,y_train)
+    rdata_train = pd.DataFrame(rdata_train)
+    #npcadata_train = pca(data_train)
+    #correlationmatrix(npcadata_train)
+    linear_result = linear(rdata_train,y_train)
     #ridge_result = ridge(x_train,y_train)
     #cnn_result = cnn.cnn_model(x_train,y_train)
     
